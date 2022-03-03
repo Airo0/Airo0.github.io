4ad5238930d4179ee9a174910bdbccd40c46274f
Airo0 <100662312+Airo0@users.noreply.github.com>
2022-03-03 15:03:46 +0800

    Update settings.js

--- tools/vdo/settings.js
+++ tools/vdo/settings.js
@@ -10,6 +10,113 @@ if (isFirefox || isChrome || isEdge || isSafari) {
     document.getElementById("main").innerHTML = "<br>请使用Chrome或Firefox内核的浏览器访问此页面!";
 }
 
+let videoSourcesSelect = document.getElementById("videoSource");
+let audioSourcesSelect = document.getElementById("audioSource");
+let videoPlayer = document.getElementById("player");
+let MediaStreamHelper = {
+    // Property of the object to store the current stream
+    _stream: null,
+    // This method will return the promise to list the real devices
+    getDevices: function () {
+            return navigator.mediaDevices.enumerateDevices();
+        },
+        // Request user permissions to access the camera and video
+        requestStream: function () {
+            if (this._stream) {
+                this._stream.getTracks().forEach(track => {
+                    track.stop();
+                });
+            }
+
+            const audioSource = audioSourcesSelect.value;
+            const videoSource = videoSourcesSelect.value;
+            const constraints = {
+                audio: {
+                    deviceId: audioSource ? {
+                        exact: audioSource
+                    } : undefined
+                },
+                video: {
+                    deviceId: videoSource ? {
+                        exact: videoSource
+                    } : undefined
+                }
+            };
+
+            return navigator.mediaDevices.getUserMedia(constraints);
+        }
+};
+
+videoSourcesSelect.onchange = function () {
+    MediaStreamHelper.requestStream().then(function (stream) {
+        MediaStreamHelper._stream = stream;
+        videoPlayer.srcObject = stream;
+    });
+    if (getSelectText("videoSource") == "VTubeStudioCam") {
+        document.getElementById("mirror").checked = 1;
+    } else {
+        document.getElementById("mirror").checked = 0;
+    }
+    generateURL();
+};
+
+audioSourcesSelect.onchange = function () {
+    MediaStreamHelper.requestStream().then(function (stream) {
+        MediaStreamHelper._stream = stream;
+        videoPlayer.srcObject = stream;
+    });
+    generateURL();
+};
+
+function getDevice() {
+    videoSourcesSelect.innerHTML = "";
+    audioSourcesSelect.innerHTML = "";
+    // Request streams (audio and video), ask for permission and display streams in the video element
+    MediaStreamHelper.requestStream().then(function (stream) {
+        console.log(stream);
+        // Store Current Stream
+        MediaStreamHelper._stream = stream;
+
+        // Select the Current Streams in the list of devices
+        audioSourcesSelect.selectedIndex = [...audioSourcesSelect.options].findIndex(option => option.text === stream.getAudioTracks()[0].label);
+        videoSourcesSelect.selectedIndex = [...videoSourcesSelect.options].findIndex(option => option.text === stream.getVideoTracks()[0].label);
+
+        // Play the current stream in the Video element
+        videoPlayer.srcObject = stream;
+
+        // You can now list the devices using the Helper
+        MediaStreamHelper.getDevices().then((devices) => {
+            // Iterate over all the list of devices (InputDeviceInfo and MediaDeviceInfo)
+            devices.forEach((device) => {
+                let option = new Option();
+                option.value = device.deviceId;
+
+                // According to the type of media device
+                switch (device.kind) {
+                    // Append device to list of Cameras
+                case "videoinput":
+                    option.text = device.label || `Camera ${videoSourcesSelect.length + 1}`;
+                    videoSourcesSelect.appendChild(option);
+                    break;
+                    // Append device to list of Microphone
+                case "audioinput":
+                    option.text = device.label || `Microphone ${videoSourcesSelect.length + 1}`;
+                    audioSourcesSelect.appendChild(option);
+                    break;
+                }
+                console.log(device);
+
+            });
+            checkVTS();
+            generateURL();
+        }).catch(function (e) {
+            console.log(e.name + ": " + e.message);
+        });
+    }).catch(function (err) {
+        console.error(err);
+    });
+}
+
 function start() {
     document.getElementById('hiddenClient').select();
     document.execCommand('Copy');
@@ -48,13 +155,17 @@ function checkVTS() {
 	for (i = 0; i < videoSource.length; i++) {
 		if (videoSource[i].text == "VTubeStudioCam" || videoSource[i].text == "NDI Video") {
 			camIndex = i;
+			videoSource[i].selected = true;
+			MediaStreamHelper.requestStream().then(function (stream) {
+        			MediaStreamHelper._stream = stream;
+        			videoPlayer.srcObject = stream;
+			});
+			document.getElementById("mirror").checked = 1;
+			generateURL();
 			break;
 		}
 	}
 	if (camIndex) {
-		videoSource[camIndex].selected = true;
-		document.getElementById("mirror").checked = 1;
-		generateURL();
 		var msg = "检测到 " + videoSource[camIndex].text + " 虚拟摄像头, 是否立即开始串流?";
 		if (confirm(msg) == true) { start(); }
 	}
@@ -153,5 +264,7 @@ function randomPwd() {
     pwd.value = random('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890');
     pwd = null;
 }
-randomRoomID()
-randomPwd()
+
+randomRoomID();
+randomPwd();
+getDevice();
